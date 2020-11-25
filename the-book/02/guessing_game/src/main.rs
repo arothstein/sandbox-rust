440df285c352eb64c37ec84c09fd97341b719b0a
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-11-25 16:21:58 -0600

    Ch2 complete

--- the-book/02/guessing_game/src/main.rs
+++ the-book/02/guessing_game/src/main.rs
@@ -1,24 +1,39 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101); // inclusive on the lower bound but exclusive on the upper bound
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
+        // In Rust, variables are immutable by default. Use 'mut' to explicitly declare a variable mutable.
+        // The :: syntax indicates that new is an associated function of the String type, aka a static method.
 
-    let mut guess = String::new();
-    // In Rust, variables are immutable by default. Use 'mut' to explicitly declare a variable mutable.
-    // The :: syntax indicates that new is an associated function of the String type, aka a static method.
+        io::stdin()
+        .read_line(&mut guess) // The & indicates the argument is a reference. Also immutable by default, you need to use &mut guess rather than &guess to make it mutable.
+        .expect("Failed to read line");
 
-    io::stdin()
-    .read_line(&mut guess) // The & indicates the argument is a reference. Also immutable by default, you need to use &mut guess rather than &guess to make it mutable.
-    .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
-    // ou can print more than one value using curly brackets: the first set of curly brackets holds the first value listed after the format string,
-    // the second set holds the second value, and so on.
+        println!("You guessed: {}", guess);
+        // ou can print more than one value using curly brackets: the first set of curly brackets holds the first value listed after the format string,
+        // the second set holds the second value, and so on.
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
