d7d43fc950fd938db0e453e4fa9b7ea949f0ef7a
Aaron Rothstein <aaron.rothstein@wunita.com>
2020-12-11 14:43:46 -0600

    Finished chapter 06

---
+++ the-book/06/matching/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let x = Coin::Penny;
+    println!("{}", value_in_cents(x));
+
+    // The _ Placeholder
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
+
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
